9b4f1fb0b0aa94d91a7c2614aad8ce8e1801b2aa
nancyma713 <nancyma713@outlook.com>
2020-04-09 18:17:38 -0400

    more styling and added banner

--- frontend/components/session/splash.jsx
+++ frontend/components/session/splash.jsx
@@ -4,18 +4,23 @@ import EventIndexContainer from '../events/event_index_container';
 const Splash = ({ currentUser, logout }) => {
 
     return (
-        <div className="index-body">
-            <h1>All Events</h1>
-            <EventIndexContainer />
-            <div className="category-browse">
-                <h4>Browse by Category</h4>
-                <ul className="category-list">
-                    <li>Entertainment</li>
-                    <li>Food and Drink</li>
-                    <li>Free</li>
-                    <li>Music</li>
-                    <li>Other</li>
-                </ul>
+        <div className="splash-body">
+            <div className="banner-section">
+                <img id="banner" src={window.bannerURL} alt="Find events near you" />
+            </div>
+            <div className="index-body">
+                <h1>All Events</h1>
+                <EventIndexContainer />
+                <div className="category-browse">
+                    <h4>Browse by Category</h4>
+                    <ul className="category-list">
+                        <li>Entertainment</li>
+                        <li>Food and Drink</li>
+                        <li>Free</li>
+                        <li>Music</li>
+                        <li>Other</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
