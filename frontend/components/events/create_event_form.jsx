eb908b2af12bcf5d41390456814a08c4b0efcc8f
nancyma713 <nancyma713@outlook.com>
2020-04-10 00:09:08 -0400

    css edits

--- frontend/components/events/create_event_form.jsx
+++ frontend/components/events/create_event_form.jsx
@@ -37,50 +37,53 @@ class CreateEventForm extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="event-form-container">
                 <h2>Create An Event</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Event Title
-                        <input type="text"
-                        placeholder="Give it a short distinct name"
-                        value={this.state.title}
-                        onChange={this.update('title')}
-                        />
-                    </label>
-                    <label>Starts
-                        <input type="datetime-local"
-                        value={this.state.start_date}
-                        onChange={this.update('start_date')}/>
-                    </label>
-                    <label>Ends
-                        <input type="datetime-local"
-                            value={this.state.end_date}
-                            onChange={this.update('end_date')} />
-                    </label>
-                    <label>Event Description
-                        <textarea value={this.state.description} 
-                        onChange={this.update('description')}
-                        />
-                    </label>
-                    <label>Category
-                        <select value={this.state.category} onChange={this.update('category')}>
-                            <option>Select Category</option>
-                            <option value="Activities">Activities</option>
-                            <option value="Community and Culture">Community and Culture</option>
-                            <option value="Entertainment">Entertainment</option>
-                            <option value="Family and Education">Family and Education</option>
-                            <option value="Food and Drink">Food and Drink</option>
-                            <option value="Free">Free</option>
-                            <option value="Home and Lifestyle">Home and Lifestyle</option>
-                            <option value="Music">Music</option>
-                            <option value="Nightlife">Nightlife</option>
-                            <option value="Other">Other</option>
-                            <option value="Science and Technology">Science and Technology</option>
-                            <option value="Sports and Fitness">Sports and Fitness</option>
-                            <option value="Travel and Outdoor">Travel and Outdoor</option>
-                        </select>
-                    </label>
-                    {this.renderErrors()}
+                <form onSubmit={this.handleSubmit} className="event-form">
+                    <div className="event-form-content">
+                        <label id="event-title">Event Title
+                            <input type="text"
+                            placeholder="Give it a short distinct name"
+                            value={this.state.title}
+                            onChange={this.update('title')}
+                            />
+                        </label>
+                        <label id="event-start">Starts
+                            <input type="datetime-local"
+                            value={this.state.start_date}
+                            onChange={this.update('start_date')}/>
+                        </label>
+                        <label id="event-end">Ends
+                            <input type="datetime-local"
+                                value={this.state.end_date}
+                                onChange={this.update('end_date')} />
+                        </label>
+                        <label id="event-desc">Event Description
+                            <textarea value={this.state.description} 
+                            onChange={this.update('description')}
+                            />
+                        </label>
+                        <label id="event-category">Category
+                            <select value={this.state.category} onChange={this.update('category')}>
+                                <option>Select Category</option>
+                                <option value="Activities">Activities</option>
+                                <option value="Community and Culture">Community and Culture</option>
+                                <option value="Entertainment">Entertainment</option>
+                                <option value="Family and Education">Family and Education</option>
+                                <option value="Food and Drink">Food and Drink</option>
+                                <option value="Free">Free</option>
+                                <option value="Home and Lifestyle">Home and Lifestyle</option>
+                                <option value="Music">Music</option>
+                                <option value="Nightlife">Nightlife</option>
+                                <option value="Other">Other</option>
+                                <option value="Science and Technology">Science and Technology</option>
+                                <option value="Sports and Fitness">Sports and Fitness</option>
+                                <option value="Travel and Outdoor">Travel and Outdoor</option>
+                            </select>
+                        </label>
+                        {this.renderErrors()}
+                    </div>
+                    <p>Nice job! You're almost done.</p>
                     <button>Save</button>
                 </form>
             </div>
