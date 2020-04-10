002cda7ab744688bd490f7d10375b06a7f8361e1
nancyma713 <nancyma713@outlook.com>
2020-04-10 15:27:17 -0400

    test

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 class Nav extends React.Component {
     constructor(props) {
         super(props);
-
-        // this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     render() {
