2f709f80410477dc662d4c5d398e80083b7a4443
nancyma713 <nancyma713@outlook.com>
2020-04-14 18:10:31 -0400

    edited banner

--- frontend/components/session/splash.jsx
+++ frontend/components/session/splash.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import EventIndexContainer from '../events/event_index_container';
 
-const Splash = () => {
+class Splash extends React.Component {
+    
+    scrollToEvents() {
+        window.scrollTo({
+            top: 500,
+            behavior: "smooth"
+        })
+    }
 
-    return (
-        <div className="splash-body">
-            <div className="banner-section">
-                <img id="banner" src={window.bannerURL} alt="Find events near you" />
-            </div>
-            <div className="banner-headers">
-                <p id="darkness-text">When darkness is your comfort zone...</p>
-                <p>Connect through DARK events!</p>
-            </div>
-            <div className="index-body">
-                <div className="category-browse">
-                    <h1>Browse by Top Categories</h1>
-                    <EventIndexContainer />
+    render() {
+        return (
+            <div className="splash-body">
+                <div className="banner-section">
+                    <img id="banner" src={window.bannerURL} alt="Find events near you" />
+                </div>
+                <div className="banner-headers">
+                    <p id="darkness-text">When darkness is your comfort zone...</p>
+                    <p id="connect">Connect through</p>
+                    <p id="dark-events"> DARK events!</p>
+                    <button id="start-browsing-btn" onClick={() => this.scrollToEvents()}>
+                        <span>Browse Events</span><i className="fas fa-arrow-right"></i></button>
+                </div>
+                <div className="index-body">
+                    <div className="category-browse">
+                        <h1>Browse by Top Categories</h1>
+                        <EventIndexContainer />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-
-
+        );
+    }
 }
 
 export default Splash;
